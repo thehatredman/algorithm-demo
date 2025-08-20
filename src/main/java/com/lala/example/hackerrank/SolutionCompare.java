ce477199f6aca16a24a5bf5311ff51a300227f27
Jay Nguyen <jaynguyen0101@gmail.com>
2025-08-21 05:52:27 +0700

    first commit

---
+++ src/main/java/com/lala/example/hackerrank/SolutionCompare.java
@@ -0,0 +1,66 @@
+package com.lala.example.hackerrank;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultCompareAAndB {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int size = a.size();
+        List<Integer> results = new ArrayList<>();
+        int aPoint = 0;
+        int bPoint = 0;
+        for (int i = 0; i < size; i++) {
+            if (a.get(i) > b.get(i)) {
+                aPoint++;
+            } else if (a.get(i) < b.get(i)) {
+                bPoint++;
+            }
+        }
+        results.add(aPoint, bPoint);
+        return results;
+    }
+}
+
+public class SolutionCompare {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = ResultCompareAAndB.compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
---
+++ src/main/java/com/lala/utils/StringUtils.java
@@ -0,0 +1,5 @@
+package com.lala.utils;
+
+public class StringUtils {
+
+}
